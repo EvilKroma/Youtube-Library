b19b235b31962a9b2d388ba13fd0aa8125336ccd
Dorian <deneuchateldorian205@gmail.com>
2024-06-29 11:47:07 +0200

    Nodes files ?

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.css';
-import VideoList from './components/VideoList';
+import React from 'react'; // Import React library to use their functionalities
+import './App.css'; // Import App's css
+import VideoList from './components/VideoList'; // Impor VideoList component for the back-end of the app
 
-const App: React.FC = () => {
+const App: React.FC = () => { // Define main coponent as react compnent function 
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Youtube Library</h1>
+        <div className="App"> {/* CSS class for the div */}
+            <header className="App-header"> {/* Same for the header */}
+                <h1>Youtube Library</h1> {/* Display app's title */}
             </header>
-            <VideoList />
+            <VideoList /> {/* Import VideoList */}
         </div>
     );
 };
 
-export default App;
+export default App; 
